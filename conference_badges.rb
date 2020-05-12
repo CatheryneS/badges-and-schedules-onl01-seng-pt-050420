c176e7e79c496ad97d3ede3ebdf111de1e7a7c0f
Catheryne Small <catheryn.small@gmail.com>
2020-05-12 22:40:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map {|attendent| "Hello, my name is #{attendent}."}
+end
+
+def assign_rooms(attendees)
+  rooms = []
+  attendees.each.with_index(1) do |name, index| 
+    rooms.push "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+  rooms
+end
+
+def printer(attendees)
+  print assign_rooms(attendees)
+end  
